f34ad728cadb31373482611022d303cdcabfcd5f
Adrian Alcaide <adrianalkide@gmail.com>
2026-01-12 11:20:21 +0100

    Config: Enable visible browser mode (disable headless) for Selenium scraping

--- license_validator.py
+++ license_validator.py
@@ -114,7 +114,7 @@ class LicenseValidator:
             
             # Initialize driver
             chrome_options = Options()
-            chrome_options.add_argument("--headless")
+            # chrome_options.add_argument("--headless")  # Comentado para ver el proceso (Petición usuario)
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             
